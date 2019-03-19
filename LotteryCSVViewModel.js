20f855232950050385a886a536330c56ef04fd20
Jason Daniels <jdaniels@cardinalsolutions.com>
2019-03-18 22:37:17 -0400

    Update CSV view model

--- LotteryCSVViewModel.js
+++ LotteryCSVViewModel.js
@@ -1,17 +1,8 @@
-// Copyright Jason Daniels 2018 Licensed under the Academic Free License version 3.0
+// Copyright Jason Daniels 2019 Licensed under the Academic Free License version 3.0
 
 ///- Parameter fileInput: HTML Input object
 ///- Parameter onCSVArray: Function to be called when a file has been converted to an array.
 function LotteryCSVViewModel(parameters) {
-  //title displayed when file is loaded
-  //upload button displayed when there is no File
-
-  // var csvInput = document.getElementById("csvInput");
-  // var loadFromInput = function(input) {
-  //   var csvReader = new FileReader();
-  //   csvReader.onload = makeCSVPrinter(csvReader);
-  //   csvReader.readAsText(input.files[0]);
-  // };
   var csvReader = new FileReader();
   var viewModel = {
     csvArray: [],
